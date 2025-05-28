5159de7a25832b40902b1ef035d49710388b21a0
Gabriel Martins Cirino <156728951+bicirino@users.noreply.github.com>
2025-05-28 17:02:57 -0300

    Update ControladorDeEstoque.C

--- ControladorDeEstoque.C
+++ ControladorDeEstoque.C
@@ -12,18 +12,12 @@ struct produto {
 
 typedef struct produto produto;
 
-
-
-
-
 int main() {
     setlocale(LC_ALL,"pt_BR");
 
-    int i, n;
+    int i, n, a;
 
-    int res; 
-	
-	
+    int res, num; 
 	
 	printf("Bem-Vindo ao controlador de estoque! Quantos produtos deseja cadastrar no controlador? ");
     scanf("%i", &n);
@@ -32,28 +26,23 @@ int main() {
 	produto produtos[n];
 
     
-	
-	
 	for (i = 0; i < n; i++) {
         printf("\n\t->Digite o nome do produto (%i): ", i + 1);
         fgets(produtos[i].nome, 21, stdin);
         fflush(stdin); 
         
 		
-        
-        
-		printf("\n\t->Digite o valor do produto (%i): R$", i + 1);
+        printf("\n\t->Digite o valor do produto (%i): R$", i + 1);
         scanf("%f", &produtos[i].valor);
         
-
-        printf("\n\t->Digite a quantidade em estoque do produto(%i): ", i + 1);
+		
+		printf("\n\t->Digite a quantidade em estoque do produto(%i): ", i + 1);
         scanf("%i", &produtos[i].quantidade);
         getchar();
         
 
         printf("\n\t->Digite uma descrição para o produto(%i): ", i + 1);
         fgets(produtos[i].descricao, 200, stdin);
-       
        
     }
 
@@ -72,10 +61,76 @@ int main() {
 	
 	printf ("\nDigite 1 - para ALTERAR dados");
 	printf ("\nDigite 2 - para ADICIONAR produto"); 
-	printf ("\nDigite 3 - para MANTER estoque da maneira que está"); 
+	printf ("\nDigite 3 - para MANTER estoque"); 
+	printf ("\n->"); 
 	scanf("%i", &res); 
 	
-	switch res 
+	switch (res){ 
+		case 1: 
+			printf("Digite o número do produto que deseja alterar (1 a %i):", n); 
+			scanf ("%i", &i ); 
+			getchar(); 
+			
+			
+			printf("\n\t->Digite o nome do produto (%i): ", i + 1);
+        	fgets(produtos[i].nome, 21, stdin);
+        	fflush(stdin); 
+			
+			printf("\n\t->Digite o valor do produto (%i): R$", i + 1);
+        	scanf("%f", &produtos[i].valor);
+			
+			 printf("\n\t->Digite a quantidade em estoque do produto(%i): ", i + 1);
+        	scanf("%i", &produtos[i].quantidade);
+        	getchar();
+        
+
+        	printf("\n\t->Digite uma descrição para o produto(%i): ", i + 1);
+        	fgets(produtos[i].descricao, 200, stdin);
+		
+		break; 
+		
+		
+		case 2: 
+			printf ("Quantos produtos deseja acresentar?: "); 
+			scanf("%i", &a); 
+			
+			for (i=n; i < n+a; i++){
+			
+				printf("\n\t->Digite o nome do produto (%i): ", i+1 );
+        		fgets(produtos[i].nome, 21, stdin);
+        		fflush(stdin); 
+        
+				printf("\n\t->Digite o valor do produto (%i): R$", i+1 );
+	        	scanf("%f", &produtos[i].valor);
+	        
+	
+	        	printf("\n\t->Digite a quantidade em estoque do produto(%i): ", i+1 );
+	        	scanf("%i", &produtos[i].quantidade);
+	        	getchar();
+	        
+	
+	        	printf("\n\t->Digite uma descrição para o produto(%i): ", i+1 );
+	        	fgets(produtos[i].descricao, 200, stdin);
+	    	}
+		break;
+		
+		
+		case 3:
+			printf("\n--- Produtos Cadastrados ---\n");
+    		for (i = 0; i < n; i++) {
+        		printf("Produto %d:\n", i + 1);
+        		printf("  Nome: %s\n", produtos[i].nome);
+        		printf("  Valor: %.2f\n", produtos[i].valor);
+        		printf("  Quantidade: %d\n", produtos[i].quantidade);
+        		printf("  Descricao: %s\n", produtos[i].descricao);
+    		}
+		break; 
+		
+		
+		default: 
+			printf("Comando não reconhecido. Tente novamente"); 
+		break;
+	}
 	
 	
 	
